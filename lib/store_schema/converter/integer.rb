6b9d95e7931ae0198d4bc16b20c95c2a7a1981a4
Michael van Rooijen <michael@vanrooijen.io>
2015-11-26 20:10:43 +0100

    Moved the Regexp definition into a constant to avoid multiple memory allocations.

--- lib/store_schema/converter/integer.rb
+++ lib/store_schema/converter/integer.rb
@@ -2,6 +2,10 @@ require_relative "base"
 
 class StoreSchema::Converter::Integer < StoreSchema::Converter::Base
 
+  # @return [Regexp] the int value format.
+  #
+  INT_FORMAT = /^\d+$/
+
   # Converts the {#value} to a database-storable value.
   #
   # @return [String, false] false if {#value} is an invalid date-type.
@@ -11,7 +15,7 @@ class StoreSchema::Converter::Integer < StoreSchema::Converter::Base
     when ::Integer
       value.to_s
     when ::String
-      if value =~ /^\d+$/
+      if value.match(INT_FORMAT)
         value
       else
         false
