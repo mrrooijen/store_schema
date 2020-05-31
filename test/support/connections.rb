6a9375bd02396b49f138d5069b00863f1c63854e
Michael van Rooijen <michael@vanrooijen.io>
2020-05-31 15:11:03 +0200

    Upgraded MySQL to v8 and fixed connectivity issue in Github Actions.

--- test/support/connections.rb
+++ test/support/connections.rb
@@ -1,9 +1,9 @@
 CONNECTIONS = {
   :prepare => {
     :postgresql => "postgres://#{`whoami`.strip}:postgres@127.0.0.1:5432/postgres",
-    :mysql      => "mysql2://root:root@127.0.0.1:3306/store_schema",
+    :mysql      => "mysql2://root:root@127.0.0.1:#{ENV["MYSQL_PORT"] || 3306}/store_schema",
   },
   :postgresql => "postgres://#{`whoami`.strip}:postgres@127.0.0.1:5432/store_schema",
-  :mysql      => "mysql2://root:root@127.0.0.1:3306/store_schema",
+  :mysql      => "mysql2://root:root@127.0.0.1:#{ENV["MYSQL_PORT"] || 3306}/store_schema",
   :sqlite3    => "sqlite3:tmp/store_schema.sqlite3",
 }
